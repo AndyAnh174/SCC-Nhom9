9e1888f2e51b7b9f68dbdf596f9fecafd868c65b
darktheDE <162719308+darktheDE@users.noreply.github.com>
2024-07-12 19:55:23 +0700

    update: Operator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+class Operator{    
+    char operation = operationInput[0];
+    Calculator calculator = new Calculator (num1, num2);
+    double result = 0;
+    switch (operation){
+        case '+':
+            result = calculator.Add();
+            break;
+        case '-':
+            result = calculator.Subtract();
+            break;
+        case '*':
+            result = calculator.Multiply();
+            break;
+        case '/':
+            try
+            {
+                result = calculator.Divide();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            break;
+        default:
+            Console.WriteLine("Phép toán không hợp lệ.");
+            return;
+    }
+    Console.WriteLine($"Kết quả: {num1} {operation} {num2} = {result});
+
+}
