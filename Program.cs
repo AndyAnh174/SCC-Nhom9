d17372f91f5b3bbb385ee19e7bc5be1904a2b1cd
hnhung <ngthhongnhung0405@gmail.com>
2024-07-12 19:06:29 +0700

    update:main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nhập số thứ nhất: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Nhập số thứ hai: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Chọn phép toán (+, -, *, /): ");
+        string operationInput = Console.ReadLine();
+        if (string.IsNullOrEmpty(operationInput))
+        {
+            Console.WriteLine("Phép toán không hợp lệ.");
+            return;
+        }
+    }
+}
+
