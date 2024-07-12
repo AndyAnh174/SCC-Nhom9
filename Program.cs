a19a74ed405bf2ba22fd70168feb85b973d91a65
NTNT24 <nguyenthuy20052405@gmail.com>
2024-07-12 19:02:14 +0700

    update:calculation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Calculator
+{
+
+    private double num1;
+    private double num2;
+
+    public Calculator(double num1, double num2)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double Add()
+    {
+        return num1 + num2;
+    }
+
+    public double Subtract()
+    {
+        return num1 - num2;
+    }
+
+    public double Multiply()
+    {
+        return (num1 * num2);
+    }
+
+    public double Divide()
+    {
+        if (num2 == 0)
+        {
+            throw new DivideByZeroException("Lỗi: Không thể chia cho 0");
+        }
+        return num1 / num2;
+    }
+}
